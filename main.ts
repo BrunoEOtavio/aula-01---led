5965d10b22e5cf7cbcd7ca97ef72082cb9b4a06d
BrunoEOtavio <125394500+BrunoEOtavio@users.noreply.github.com>
2023-02-14 15:33:27 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+	///Otavio:
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    basic.showIcon(IconNames.SmallHeart);
+    basic.pause(2500)
+    basic.clearScreen()
+
+    ///Bruno;
+    basic.showIcon(IconNames.Happy,2000);
+    basic.showString("Blz");
+})
